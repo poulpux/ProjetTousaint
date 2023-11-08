cc047ffbee245cd25459f89e3da2ef338f96ca59
Poulpux <114729806+poulpux@users.noreply.github.com>
2023-11-08 14:11:06 +0100

    La detection des ennemis marche

---
+++ Assets/Scripts/Ennemy/VisionField.cs
@@ -0,0 +1,149 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class VisionField : MonoBehaviour
+{
+    public Material VisionConeMaterial;
+    public float VisionRange;
+    public float VisionAngle;
+    public LayerMask VisionObstructingLayer;//layer with objects that obstruct the enemy view, like walls, for example
+    public int VisionConeResolution = 120;//the vision cone will be made up of triangles, the higher this value is the pretier the vision cone will be
+    Mesh VisionConeMesh;
+    MeshFilter MeshFilter_;
+    //Create all of these variables, most of them are self explanatory, but for the ones that aren't i've added a comment to clue you in on what they do
+    //for the ones that you dont understand dont worry, just follow along
+
+    [SerializeField] private float timeToDetect;
+
+    private float timerDetect;
+
+    private Infiltration playerInfiltration;
+    void Start()
+    {
+        playerInfiltration = FindFirstObjectByType<Infiltration>();
+        playerInfiltration.Reperated.AddListener(() =>
+        {
+            VisionConeMesh.Clear();
+            this.enabled = false;
+        });
+        transform.AddComponent<MeshRenderer>().material = VisionConeMaterial;
+        MeshFilter_ = transform.AddComponent<MeshFilter>();
+        VisionConeMesh = new Mesh();
+        VisionAngle *= Mathf.Deg2Rad;
+
+    }
+
+
+    void Update()
+    {
+        ReperatPlayer();
+        //DrawVisionCone();//calling the vision cone function everyframe just so the cone is updated every frame
+    }
+
+    void DrawVisionCone()//this method creates the vision cone mesh
+    {
+        int[] triangles = new int[(VisionConeResolution - 1) * 3];
+        Vector3[] Vertices = new Vector3[VisionConeResolution + 1];
+        Vertices[0] = Vector3.zero;
+        float Currentangle = -VisionAngle / 2;
+        float angleIcrement = VisionAngle / (VisionConeResolution - 1);
+        float Sine;
+        float Cosine;
+
+        for (int i = 0; i < VisionConeResolution; i++)
+        {
+            Sine = Mathf.Sin(Currentangle);
+            Cosine = Mathf.Cos(Currentangle);
+            Vector3 RaycastDirection = (transform.forward * Cosine) + (transform.right * Sine);
+            Vector3 VertForward = (Vector3.forward * Cosine) + (Vector3.right * Sine);
+            if (Physics.Raycast(transform.position, RaycastDirection, out RaycastHit hit, VisionRange, VisionObstructingLayer))
+            {
+                Vertices[i + 1] = VertForward * hit.distance;
+            }
+            else
+            {
+                Vertices[i + 1] = VertForward * VisionRange;
+            }
+
+
+            Currentangle += angleIcrement;
+        }
+        for (int i = 0, j = 0; i < triangles.Length; i += 3, j++)
+        {
+            triangles[i] = 0;
+            triangles[i + 1] = j + 1;
+            triangles[i + 2] = j + 2;
+        }
+        VisionConeMesh.Clear();
+        VisionConeMesh.vertices = Vertices;
+        VisionConeMesh.triangles = triangles;
+        MeshFilter_.mesh = VisionConeMesh;
+    }
+
+    private void detectPlayer()
+    {
+        int[] triangles = new int[(VisionConeResolution - 1) * 3];
+        Vector3[] Vertices = new Vector3[VisionConeResolution + 1];
+        Vertices[0] = Vector3.zero;
+        float Currentangle = -VisionAngle / 2;
+        float angleIncrement = VisionAngle / (VisionConeResolution - 1);
+        float Sine;
+        float Cosine;
+        bool playerDetected = false;
+
+        for (int i = 0; i < VisionConeResolution; i++)
+        {
+            Sine = Mathf.Sin(Currentangle);
+            Cosine = Mathf.Cos(Currentangle);
+            Vector3 RaycastDirection = (transform.forward * Cosine) + (transform.right * Sine);
+            Vector3 VertForward = (Vector3.forward * Cosine) + (Vector3.right * Sine);
+            if (Physics.Raycast(transform.position, RaycastDirection, out RaycastHit hit, VisionRange, VisionObstructingLayer))
+            {
+                //timerDetect += Time.deltaTime;
+                if (hit.collider.CompareTag("Player"))
+                {
+                    playerDetected = true;
+                }
+                Vertices[i + 1] = VertForward * hit.distance;
+
+                // Vérifier si l'objet touché est le joueur
+            }
+            else
+            {
+                Vertices[i + 1] = VertForward * VisionRange;
+            }
+
+            Currentangle += angleIncrement;
+        }
+
+        for (int i = 0, j = 0; i < triangles.Length; i += 3, j++)
+        {
+            triangles[i] = 0;
+            triangles[i + 1] = j + 1;
+            triangles[i + 2] = j + 2;
+        }
+
+        VisionConeMesh.Clear();
+        VisionConeMesh.vertices = Vertices;
+        VisionConeMesh.triangles = triangles;
+        MeshFilter_.mesh = VisionConeMesh;
+
+        if (playerDetected)
+        {
+            timerDetect += Time.deltaTime;
+            // Faites quelque chose si le joueur est détecté
+        }
+
+    }
+        private void ReperatPlayer()
+    {
+        detectPlayer();
+        if(timerDetect > timeToDetect)
+        {
+            playerInfiltration.Reperated.Invoke();
+        }
+    }
+
+}
